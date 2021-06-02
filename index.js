5a91653cc65af9285ce509ff5dc55cdcb23306f5
Unusual-guy <81540865+Unusual-guy@users.noreply.github.com>
2021-06-03 02:05:20 +0700

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./secret/serviceAccountKey');
+const serviceAccount = require('./secret/serviceAccountKey.json');
 const { lowerCase } = require('lodash');
 const { INTENT, getIntent } = require('./src/nlu');
 const lex = require('./src/lexicon');
@@ -52,11 +52,48 @@ module.exports.skill = async (event) => {
     });
 
 
+    // Инициализируем переменные, которые будут принимать значения из базы данных
+    let didDevice = 0;
+    let dday = 0;
+    let doutput_active_power = 0;
+    let dbattery_capacity = 0;
+    let dsolar_panel_input_power = 0;
+    let dstatus = 0;
+    let dgenerge_total = 0;
+    let dgenerge_year = 0;
+    let dgenerge_month = 0;
+    let dgenerge_day = 0;
+    let denerge_fPhase = 0;
+    let denerge_sPhase = 0;
+    let denerge_tPhase = 0;
+
+
+    // Получаем конкретные данные из БД 
+    if(!isEmpty(userData)) {
+      didDevice = userData && userData.dat;
+      dday = userData && userData.dat;
+      doutput_active_power = userData && userData.dat;
+      dbattery_capacity = userData && userData.dat;
+      dsolar_panel_input_power = userData && userData.dat;
+      dstatus = userData && userData.dat;
+      dgenerge_total = userData && userData.dat;
+      dgenerge_year = userData && userData.dat;
+      dgenerge_month = userData && userData.dat;
+      dgenerge_day = userData && userData.dat;
+      denerge_fPhase = userData && userData.dat;
+      denerge_sPhase && userData.dat;
+      denerge_tPhase && userData.dat;
+    }
 
 
 
 
     // ДИАЛОГ С ПОЛЬЗОВАТЕЛЕМ:
+    let msg = ''; // переменная, в которой будет храниться ответ Алисы пользователю (полный ответ: реплика Алисы, конкретные данные из БД)
+    
+    msg = '${sample(lex.memoryFull1)} ${spokenTime}. ${lex.memoryFull2} ${memory} ${lex.promptForget}'
+    
+    
     if (intent === INTENT.give_idDevice)
 
     ......
